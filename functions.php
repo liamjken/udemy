d621b6ede05f96311400114dff42600b619c15b9
liamjken <liamjken@gmail.com>
2023-02-15 20:22:47 -0500

    replace functions

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ include(get_theme_file_path('/includes/front/enqueue.php'));
 include(get_theme_file_path('/includes/front/head.php'));
 include(get_theme_file_path('/includes/setup.php'));
 include(get_theme_file_path('/includes/class-tgm-plugin-activation.php'));
+include(get_theme_file_path('/includes/register-plugins.php'));
 
 // Hooks
 add_action('wp_enqueue_scripts','u_enqueue');
